bcf667461ebe40f1b74abcbd90c7bfdce0787d87
MohAlkurdi <imohkurdi@gmail.com>
2023-09-27 16:35:20 +0300

    fix: sidebar not closing

--- components/CreateCollectionBtn.tsx
+++ components/CreateCollectionBtn.tsx
@@ -1,25 +1,29 @@
 "use client";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import CreateCollectionSidebar from "./CreateCollectionSidebar";
-const CreateCollectionBtn = () => {
-  const [open, setOpen] = useState(false);
 
+function CreateCollectionBtn() {
+  const [open, setOpen] = useState(false);
   const handleOpenChange = (open: boolean) => setOpen(open);
 
   return (
-    <Button
-      variant={"outline"}
-      className="dark:text-white w-full dark:bg-neutral-950 bg-white"
-      onClick={() => setOpen(true)}
+    <div
+      className="
+    w-full rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[1px]"
     >
-      <span className="bg-gradient-to-r from-pink-500 to-red-400 hover:to-red-800 bg-clip-text text-transparent">
-        Create collection
-      </span>
-
+      <Button
+        variant={"outline"}
+        className="dark:text-white w-full dark:bg-neutral-950 bg-white"
+        onClick={() => setOpen(true)}
+      >
+        <span className="bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent">
+          Create collection
+        </span>
+      </Button>
       <CreateCollectionSidebar open={open} onOpenChange={handleOpenChange} />
-    </Button>
+    </div>
   );
-};
+}
 
 export default CreateCollectionBtn;
